5452c17db6c8ab343644ad745bbc690d2a460272
EAR <sinedie@protonmail.com>
2023-02-21 22:16:04 -0500

    Minor version updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/sinedie/SRTranslator",
-    version="0.1.92",
+    version="0.1.93",
     author="EAR",
     author_email="sinedie@protonmail.com",
     license="FREE",
