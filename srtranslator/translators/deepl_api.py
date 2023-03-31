131c2d17f9dd6e9b04f9397b30d485d11a29f0fe
EAR <sinedie@protonmail.com>
2023-03-31 09:58:53 -0500

    typo on deepl-api translator

--- srtranslator/translators/deepl_api.py
+++ srtranslator/translators/deepl_api.py
@@ -9,7 +9,7 @@ class DeeplApi(Translator):
         self.translator = deepl.Translator(api_key)
 
     def translate(self, text: str, source_language: str, destination_language: str):
-        result = translator.translate_text(
+        result = self.translator.translate_text(
             text, source_lang=source_language, target_lang=destination_language
         )
         return result.text
