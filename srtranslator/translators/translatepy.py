af09d5b86b81b631df5a359c32adfd006229d42c
EAR <sinedie@protonmail.com>
2023-04-23 10:51:45 -0500

    Don't handle translatepy errors

--- srtranslator/translators/translatepy.py
+++ srtranslator/translators/translatepy.py
@@ -11,20 +11,9 @@ class TranslatePy(BaseTranslator):
         self.translator = Translator()
 
     def translate(self, text, source_language, destination_language):
-        try:
-            result = self.translator.translate(
-                text,
-                source_language=source_language,
-                destination_language=destination_language,
-            )
-            return result.result
-        except UnknownLanguage as err:
-            print("An error occured while searching for the language you passed in")
-            print("Similarity:", round(err.similarity), "%")
-            return
-        except TranslatepyException:
-            print("An error occured while translating with translatepy")
-            return
-        except Exception:
-            print("An unknown error occured")
-            return
+        result = self.translator.translate(
+            text,
+            source_language=source_language,
+            destination_language=destination_language,
+        )
+        return result.result
